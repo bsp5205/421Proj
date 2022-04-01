bfee4a8c585e275752ce04f87be3188cd5a55299
Shotzwastaken <bsp13101@gmail.com>
2022-04-01 14:36:44 -0400

    button action

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get("/", function(req,res){
     res.render("index", {title:"This is a title",message:"This is a message"});
 });
+app.get("/test", function(req,res){
+    res.render("test", {title:"This is a title",message:"This is a message"});
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
