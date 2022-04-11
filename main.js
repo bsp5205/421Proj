a0a48806d46701a10d44419a40480b7e32992298
ChrisMamatas <chrismamatas@yahoo.com>
2022-04-11 15:07:13 -0400

    added database connection

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 var path = require('path');
 const response = require("express");
 const createError = require("http-errors");
-const sql = require('mysql');
+const mysql = require('mysql');
 
 
 const app = express();
@@ -21,6 +21,24 @@ app.get("/login", (req, res) => {
     res.render("login.ejs", {title: "FUBAR | Login"});
 });
 
+app.get("/register", (req, res) => {
+    res.render("register.ejs", {title: "FUBAR | Register"});
+});
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
+});
+
+
+// Connect to the database
+
+var con = mysql.createConnection({
+    host     : 'fubar.c15l35ljlxyx.us-east-1.rds.amazonaws.com',
+    user     : 'admin',
+    password : 'password'
+});
+  
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
 });
