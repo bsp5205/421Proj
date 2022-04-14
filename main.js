65703fc48bf9ae6a31eed54a09c4c560c1f54788
ChrisMamatas <chrismamatas@yahoo.com>
2022-04-13 21:47:27 -0400

    login working with database, login with admin admin

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ var passport = require('passport');
 var bodyParser = require('body-parser');
 var localStrategy = require('passport-local').Strategy;
 
-var UserName = "";
+//var UserName = "";
 
 const app = express();
 const port = 3000;
 
-var UserList = [{username:'admin',password:'admin'}, {username:'bpm5520',password:'pass'}, {username:'Shotz',password:'Val'}];
+//var UserList = [{username:'admin',password:'admin'}, {username:'bpm5520',password:'pass'}, {username:'Shotz',password:'Val'}];
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(passport.initialize());
@@ -22,37 +22,59 @@ app.use(passport.initialize());
 app.set("views",path.resolve(__dirname,"views"));
 app.set("view engine","ejs");
 
-passport.use('local', new localStrategy(
-    function(username, password, done) {
+// passport.use('local', new localStrategy(
+//     function(username, password, done) {
 
-        var Check_user = username;
-        var Info = UserList.find((element) => {
-            if (element.username == Check_user) {
-                UserName = element.username;
-                return element;
-            }
-        });
+//         var Check_user = username;
+//         var Info = UserList.find((element) => {
+//             if (element.username == Check_user) {
+//                 UserName = element.username;
+//                 return element;
+//             }
+//         });
 
-        console.log("Auth found this " + Info);
-        if (Info == undefined || Info == null) {
-            return done(null, false, {message: 'Incorrect username'});
-        }
-        if (Info.password != password) {
-            return done(null, false, {message: 'Incorrect password'});
-        } else {
-            console.log("Accepted");
-            return done(null, Info);
-        }
-    }
-));
+//         console.log("Auth found this " + Info);
+//         if (Info == undefined || Info == null) {
+//             return done(null, false, {message: 'Incorrect username'});
+//         }
+//         if (Info.password != password) {
+//             return done(null, false, {message: 'Incorrect password'});
+//         } else {
+//             console.log("Accepted");
+//             return done(null, Info);
+//         }
+//     }
+// ));
 
-app.post('/login', passport.authenticate('local',{
-        failureRedirect: "/login-failed",
-        session: false}),
-        function(req,res){
-            res.render("profile.ejs", {title: "FUBAR | Testing", username: UserName});
+// app.post('/login', passport.authenticate('local',{
+//         failureRedirect: "/login-failed",
+//         session: false}),
+//         function(req,res){
+//             res.render("profile.ejs", {title: "FUBAR | Testing", username: UserName});
+//         }
+// );
+
+// Login authentication
+app.post('/login', (req, res) => {
+    let user = req.body.username;
+    let pass = req.body.password;
+
+    // Check if entries are valid
+    if (user && pass) { // Checks if values are not empty
+        con.query('SELECT * FROM login WHERE user = ? AND pass = ?', [user, pass], function(error, results, fields) { // Run the query
+        if (error) throw error;
+
+        // If the login exists
+        if (results.length > 0){
+            // Log the user in
+            res.render('profile.ejs', {title: 'FUBAR | ' + user, username: user})
         }
-);
+        else {
+            res.send('Incorrect username or password');
+        }
+    res.send();
+    })
+}})
 
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -102,8 +124,10 @@ app.listen(port, () => {
 
 var con = mysql.createConnection({
     host     : 'fubar.c15l35ljlxyx.us-east-1.rds.amazonaws.com',
+    port     : '3306',
     user     : 'admin',
-    password : 'password'
+    password : 'password',
+    database : 'fubar'
 });
   
 con.connect(function(err) {
